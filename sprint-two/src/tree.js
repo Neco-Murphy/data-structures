139e3e31d8b4920cb54b9ab48fe70e654aef0e85
Satoko Lom <stogamori@gmail.com>
2014-09-12 20:59:24 -0700

    complete tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,6 +2,7 @@ var makeTree = function(value){
   var newTree = {};
   newTree.value = value;
   newTree.children = undefined;
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
@@ -11,11 +12,28 @@ var makeTree = function(value){
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
+	if(!this.children){
+    this.children = [];
+	}
+	this.children.push(makeTree(value));
 
 };
 
 treeMethods.contains = function(target){
-
+	var result = false;
+  var check = function(tree){
+    if(tree.value === target){
+    	result = true;
+    	return result;
+    }
+    if(tree.children){
+      for(var i = 0; i < tree.children.length; i++){
+      	check(tree.children[i]);
+      }
+    }
+  }
+  check(this);
+  return result;
 };
 
 
